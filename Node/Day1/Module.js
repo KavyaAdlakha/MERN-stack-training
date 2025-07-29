b075963ebf8efe4789ec66c2e43d36978ba1227c
kavya <kavye14@gmail.com>
2025-07-29 23:58:32 +0530

    Node

---
+++ Node/Day1/Module.js
@@ -0,0 +1,8 @@
+const add = (a,b) => {
+    return(a+b)
+} 
+const diff = (a,b) => {
+    return(a-b)
+}
+module.exports.add = add;
+module.exports.diff = diff;
---
+++ Node/Day1/index.js
@@ -0,0 +1,36 @@
+--->  Sync method
+
+const fs=require("fs")
+buff_data = fs.readFileSync("hello.txt")
+Data = buff_data.toString()
+console.log(Data)
+fs.renameSync("hello.txt","hii.txt")
+fs.unlinkSync("hii.txt")
+
+---> Async method
+
+
+const fs=require("fs")
+fs.writeFile("hello.txt","this is example of async",
+    (err)=>{
+        console.log("new file creates")
+        console.log(err)
+    }
+)
+fs.readFile("hello.txt","utf-8",
+    (err,data)=> {
+        console.log(data)
+        console.log(err)
+    }
+)
+
+---> OPerating system os
+
+const os = require("os")
+os.
+
+Build Own module----------------------
+
+const oper = require("./Module")
+console.log(oper.add(5,6))
+console.log(oper.diff(10,7))
