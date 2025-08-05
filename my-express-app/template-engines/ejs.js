e2022a89f9445f10d0cf78b088d6a8d0b1f52a0d
KavyaAdlakha <kavye14@gmail.com>
2025-08-05 22:16:55 +0530

    template

---
+++ my-express-app/template-engines/ejs.js
@@ -0,0 +1,14 @@
+import express from 'express';
+
+const app = express();
+
+app.set('view engine', 'ejs');  //set EJS as the template engine
+app.set('views', '../Views');   // specify the view folder
+
+app.get('/', (req,res) => {
+    res.render('index', {title: 'Hello EJS', message: 'This is  Ejs'})
+});
+
+app.listen(3000, ()=> 
+    console.log('server running on http://localhost:3000')
+)
